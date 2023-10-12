67637c929409c4e4d4684989270b45121c04f118
panzas624 <57840765+panzas624@users.noreply.github.com>
2023-10-11 20:27:02 -0600

    Create panzas624.py

---
+++ Retos/Reto #40 - TABLA DE MULTIPLICAR [Fácil]/python/panzas624.py
@@ -0,0 +1,14 @@
+def tablamultiplicar(numero: int):
+
+    if numero < 1 or numero > 10:
+       print(f"No se puede procesar el numero {numero}")
+    else:   
+       for a in range(1, 11):
+           print(f"{numero} x {a} = {numero * a}")
+
+tablamultiplicar(2)
+tablamultiplicar(11)
+tablamultiplicar(5)
+tablamultiplicar(0)
+tablamultiplicar(10)
+tablamultiplicar(12)
